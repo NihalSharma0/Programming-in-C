4605ba3ac97b39867339044c30cec4dea100f173
NihalSharma0 <nihalsharma0507@gmail.com>
2025-10-28 12:10:23 +0530

    Add files via upload

---
+++ Experiment 6/Experiment 6.3.c
@@ -0,0 +1,32 @@
+//6.3. WAP to read a list of integers and store it in a single dimensional array. Write a Cprogram to find the frequency of a particular number in a list of integers
+
+#include <stdio.h>
+
+int main()
+{
+    int n, num, i, count = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d integers:\n", n);
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter number to find frequency: ");
+    scanf("%d", &num);
+
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] == num)
+            count++;
+    }
+
+    printf("Frequency of %d = %d\n", num, count);
+
+    return 0;
+}
